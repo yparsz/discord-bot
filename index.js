b521df008d8137d1566b2cebebd43f4dc533ea52
yparsz <brp3459@gmail.com>
2026-05-13 18:20:51 +1200

    index.js

--- index.js
+++ index.js
@@ -14,14 +14,16 @@ const client = new Client({
 
 client.commands = new Collection();
 
-const commandsPath = path.join(__dirname, 'commands');
-const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+if (fs.existsSync(path.join(__dirname, 'commands'))) {
+  const commandsPath = path.join(__dirname, 'commands');
+  const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
 
-for (const file of commandFiles) {
-  const filePath = path.join(commandsPath, file);
-  const command = require(filePath);
+  for (const file of commandFiles) {
+    const filePath = path.join(commandsPath, file);
+    const command = require(filePath);
 
-  client.commands.set(command.data.name, command);
+    client.commands.set(command.data.name, command);
+  }
 }
 
 client.once(Events.ClientReady, () => {
