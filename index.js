3db07bb92f9ad4eb443abad496bcf1fa52f74500
yparsz <brp3459@gmail.com>
2026-05-13 18:12:11 +1200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,53 @@
+const fs = require('fs');
+const path = require('path');
+
+const {
+  Client,
+  Collection,
+  GatewayIntentBits,
+  Events
+} = require('discord.js');
+
+const config = require('./config.json');
+
+const client = new Client({
+  intents: [GatewayIntentBits.Guilds]
+});
+
+client.commands = new Collection();
+
+const commandsPath = path.join(__dirname, 'commands');
+const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+  const filePath = path.join(commandsPath, file);
+  const command = require(filePath);
+
+  client.commands.set(command.data.name, command);
+}
+
+client.once(Events.ClientReady, () => {
+  console.log(`Logged in as ${client.user.tag}`);
+});
+
+client.on(Events.InteractionCreate, async interaction => {
+
+  if (!interaction.isChatInputCommand()) return;
+
+  const command = client.commands.get(interaction.commandName);
+
+  if (!command) return;
+
+  try {
+    await command.execute(interaction);
+  } catch (error) {
+    console.error(error);
+
+    await interaction.reply({
+      content: 'There was an error.',
+      ephemeral: true
+    });
+  }
+});
+
+client.login(config.token);
